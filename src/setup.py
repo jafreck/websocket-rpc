3fc1d078bbd8d8944b38185433d3a0fb403bc92c
Jake Fredk <jafreck@microsoft.com>
2020-10-05 10:50:19 -0700

    fix publishing

--- src/setup.py
+++ src/setup.py
@@ -1,3 +1,19 @@
-from setuptools import setup, find_packages
+import setuptools
 
-setup(name="websocket-rpc", packages=find_packages())
+
+setuptools.setup(
+    name="websocket-rpc",
+    version="0.0.1",
+    author="jafreck",
+    author_email="jafreck@microosft.com",
+    description="RPC over websockets",
+    long_description_content_type="text/markdown",
+    url="https://github.com/jafreck/websocket-rpc",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
